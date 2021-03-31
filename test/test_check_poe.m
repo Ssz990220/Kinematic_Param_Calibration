1349566d5c02d839c71142f639963fadb60a8df0
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2021-03-31 12:51:25 +0800

    Update Test

--- test/test_check_poe.m
+++ test/test_check_poe.m
@@ -1,8 +1,11 @@
 clear;
 clc;
 addpath(genpath('..\'));
-robot = my_new_dh_robot(eye(4));
-robot_poe = my_poe_robot(eye(4));
+robot = my_new_dh_robot();
+R_ = [-1,0,0;0,1,0;0,0,-1]';
+T_tool= [R_,[0,0,370]';
+        zeros(1,3),1];
+robot_poe = my_poe_robot(T_tool);
 %% Denormalize the axis for poe robot to validate algorithm
 for i = 1:robot_poe.n_dof
     robot_poe.links(1:3,i) = robot_poe.links(1:3,i)*i;  
--- test/test_poe_calib_pos.m
+++ test/test_poe_calib_pos.m
@@ -1,18 +1,23 @@
 clear;
 clc;
-T0 = [eye(3),[1000,0,1300]';
-            zeros(1,3),1];
-dis =  50 * ones(6,1);
-direction = [1,0,0]';
-measure_per_point = 3;
-n_points = 7;
-r = 50;
-[Ts, T_holes, p_measures] = gen_poe_cal_pos(T0, direction, dis, measure_per_point, n_points, r, 15, 10);
+n_holes = 20;
+T_holes = repmat(eye(4),1,1,n_holes);
+T_holes_x = randi([500,1000],1,n_holes);
+T_holes_y = randi([-500,500],1,n_holes);
+T_holes_z = randi([800,1300],1,n_holes);
+T_holes(1:3,4,:) = [T_holes_x;T_holes_y;T_holes_z];
+measure_per_point = 1;
+r = 20;
+z_angle = 45;
+[Ts, p_measures] = gen_poe_cal_pos(T_holes, measure_per_point, r, z_angle, 10);
+%% view holes and p_measure
 figure
 hold on
 axis equal
 view_holes(T_holes,10,false);
 view_measure_pose(Ts, p_measures, 10, false);
+
+%% Visualize on real robot
 robot = my_new_dh_robot();
 qs = robot.ikine(Ts);
 robot_view_generate_pose(robot, qs);

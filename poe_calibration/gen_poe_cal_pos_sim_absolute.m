30b0f61e872d70703d33a7f83a57eadfc1e5fa5e
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2021-04-03 22:49:24 +0800

    Delete gen_poe_cal_pos_sim_absolute.m

--- poe_calibration/gen_poe_cal_pos_sim_absolute.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = gen_poe_cal_pos_sim_absolute(num_point, n_dof)
-%GEN_POE_CAL_POS_SIM_ABSOLUTE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-

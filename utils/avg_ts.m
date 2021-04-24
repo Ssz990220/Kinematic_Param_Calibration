12571f7fff6acd9f14dc5c72fe57a61526df170a
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2021-04-24 16:42:51 +0800

    Upgraded avg functions

--- utils/avg_ts.m
+++ utils/avg_ts.m
@@ -1,13 +1,31 @@
-function avg= avg_ts(Ts)
+function avg= avg_ts(Ts, n_points)
 %AVG_TS Summary of this function goes here
 %   Detailed explanation goes here
-n = size(Ts,3);
-twists = zeros(6,n);
-for i = 1:n
-    se3 = MatrixLog6(Ts(:,:,i));
-    twists(:,i) = se3ToVec(se3);
+    if nargin < 2
+        avg = avg_one_ts(Ts);
+    else
+        n = size(Ts,3);
+        avg = zeros(4,4,n_points);
+        n_measures = n/n_points;
+        for i = 1:n_points
+            Ts_local = zeros(4,4,n_measures);
+            for j = 1:n_measures
+                Ts_local(:,:,j) = Ts(:,:,i+(j-1)*n_points);
+            end
+            avg(:,:,i) = avg_one_ts(Ts_local);
+        end
+    end
 end
-avg_twist = mean(twists,2);
-avg = MatrixExp6(VecTose3(avg_twist));
+
+function avg_T = avg_one_ts(Ts)
+    n_Ts = size(Ts,3);
+    twists = zeros(6,n_Ts);
+    for i = 1:n_Ts
+        se3 = MatrixLog6(Ts(:,:,i));
+        twists(:,i) = se3ToVec(se3);
+    end
+    avg_twist = mean(twists,2);
+    avg_T = MatrixExp6(VecTose3(avg_twist));
 end
+
 
--- utils/avg_ts_p.m
+++ utils/avg_ts_p.m
@@ -1,19 +1,7 @@
-function [avg_Ts, avg_p] = avg_ts_p(Ts, p_measure, n_points)
+function [Ts, ps] = avg_ts_p(Ts, p_measure, n_points)
 %AVG_TS_P Summary of this function goes here
 %   Detailed explanation goes here
-n = size(p_measure,2);
-avg_p = zeros(3, n_points);
-avg_Ts = zeros(4,4,n_points);
-n_measures = n/n_points;
-for i = 1:n_points
-    p_measure_local = zeros(3,n_measures);
-    Ts_local = zeros(4,4,n_measures);
-    for j = 1:n_measures
-        p_measure_local(:,j) = p_measure(:,i+(j-1)*n_points);
-        Ts_local(:,:,j) = Ts(:,:,i+(j-1)*n_points);
-    end
-    avg_p(:,i) = mean(p_measure_local,2);
-    avg_Ts(:,:,i) = avg_ts(Ts_local);
-end    
+ps = avg_p(p_measure, n_points);
+Ts = avg_ts(Ts, n_points);
 end
 
